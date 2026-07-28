6039870cbec96998646d3d8dc1ddc15e53637326
shraddhakakade-18 <srkakade18gmail.com>
2026-07-28 13:07:19 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("===CALCULATOR===")
+
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=a+b
+print("The addition of the entered numbers is: ",c)
+d=a-b
+print("The difference between the two number is :",d)
+e=a*b
+print("The product of entered number is :",e)
+print("The Division of entered number is :",a/b)
+print("The Remainer after devision of entered number is :",a%b)

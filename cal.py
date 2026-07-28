4c2f20f828a29184fc7b8894802b910679acbd4b
Your Name <your-github-email@example.com>
2026-07-28 12:50:52 +0530

    Addition program is added

---
+++ cal.py
@@ -0,0 +1,11 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+total_sum = num1 + num2
+
+
+print(f"The sum of {num1} and {num2} is {total_sum}")
+
+
